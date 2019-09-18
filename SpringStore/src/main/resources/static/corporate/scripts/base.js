b9708154436f406a8ca0a7c74926c8dd2ac01947
Administrator <Administrator@176.201.107.13>
2019-09-18 14:55:09 +0800

    静态资源修改

--- SpringStore/src/main/resources/static/corporate/scripts/base.js
+++ SpringStore/src/main/resources/static/corporate/scripts/base.js
@@ -2,7 +2,7 @@ $(function() {
 	initPages(); //初始化头部和尾部
 })
 function initPages() {
-	$("#header").load("header.html");
-	$("#footer").load("footer.html");
+	$("#header").load("header");
+	$("#footer").load("footer");
 }
 
