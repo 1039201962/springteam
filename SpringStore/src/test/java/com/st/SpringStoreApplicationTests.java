81ac542894675b298e6dbae10f0436d85931e45f
wangxinghai1 <wangx@176.201.107.14>
2019-09-18 20:36:07 +0800

    1

--- SpringStore/src/test/java/com/st/SpringStoreApplicationTests.java
+++ SpringStore/src/test/java/com/st/SpringStoreApplicationTests.java
@@ -2,15 +2,24 @@ package com.st;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.st.springstore.car.dao.CarDao;
+import com.st.springstore.car.pojo.Car;
+import com.st.springstore.car.service.CarService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringStoreApplicationTests {
-
+	@Autowired
+    private CarService carService;
 	@Test
-	public void contextLoads() {
+	public void testCarService() {
+		int rows = carService.addCar(1, 1, 3);
+		System.out.println(rows);
+		
 	}
 
 }
