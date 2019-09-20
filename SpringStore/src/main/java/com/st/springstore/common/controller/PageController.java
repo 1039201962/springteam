5086ce52010c911a2eddf7131cd50acd9db523c7
Administrator <Administrator@176.201.107.13>
2019-09-20 14:00:47 +0800

    lyl 14.00

--- SpringStore/src/main/java/com/st/springstore/common/controller/PageController.java
+++ SpringStore/src/main/java/com/st/springstore/common/controller/PageController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
 	@RequestMapping("/{pages}")
 	public String pages(@PathVariable String pages) {
 		return pages;
