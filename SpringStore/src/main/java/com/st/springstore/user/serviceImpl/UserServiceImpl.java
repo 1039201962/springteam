33f65a0b55dcd3c6b3eee731a332e359f3b81025
FuHechen353 <348061196@qq.com>
2019-09-20 08:29:17 +0800

    1.0.2

--- SpringStore/src/main/java/com/st/springstore/user/serviceImpl/UserServiceImpl.java
+++ SpringStore/src/main/java/com/st/springstore/user/serviceImpl/UserServiceImpl.java
@@ -36,10 +36,10 @@ public class UserServiceImpl implements UserService{
     		throw new ServiceException("保存对象不能为空");
     	if(StringUtils.isEmpty(user.getUsername()))
     		throw new ServiceException("用户名不能为空");
-    	if(user.getPassword() != passwordConfirm)
-    		throw new ServiceException("密码不能为空");
-		//保存自身信息
-		userDao.userRegister(user);
+//    	if(user.getPassword() != passwordConfirm)
+//    		throw new ServiceException("密码不能为空");
+//		//保存自身信息
+//		userDao.userRegister(user);
 		return 0;
 	}
 }
